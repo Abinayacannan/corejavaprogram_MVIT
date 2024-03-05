7f588b1d87bbad79199dd45ff963d6263c3a7685
Kannan <Kannan@LAPTOP-2R5E71P0.mshome.net>
2024-03-05 16:29:16 +0530

    committed changes

--- example1/src/day1javabasics/samplejava2.java
+++ example1/src/day1javabasics/samplejava2.java
@@ -6,7 +6,7 @@ public class samplejava2 {
 		Scanner n = new Scanner(System.in);
 		int a,b,c;
 		System.out.println("enter the a and b values:");
-		a=500;
+		a=587;
 		b=60;
 		c=a+b;
 		System.out.println("addition of a+b ="+c);
