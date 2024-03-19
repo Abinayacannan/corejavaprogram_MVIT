cfb75f1b81476d4ef31b03d5a32dfbb421173047
Kannan <Kannan@LAPTOP-2R5E71P0>
2024-03-19 22:02:29 +0530

    Banking program

---
+++ example1/src/day6_Interface2/BankApplication.java
@@ -0,0 +1,21 @@
+//Main class for Bank application
+package day6_Interface2;
+public class BankApplication {
+ public static void main(String[] args) {
+     // Create a Bank instance
+     Bank bank = new BankImpl();
+
+     // Create an Account
+     Account account = new Account(123456, "Mihir", 900, bank);
+
+     // Deposit money
+     account.deposit(20);
+     // Print the account details
+     System.out.println(account);
+     
+     //withdraw money
+     account.withdraw(500);
+     // Print the account details
+     System.out.println(account);
+ }
+}
